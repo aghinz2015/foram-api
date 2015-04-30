a3f3039daae59713b3553d50e9bb81674407d2b7
Bartosz Zurkowski <b.zurkowski93@gmail.com>
2015-05-01 00:51:34 +0200

    Bootstrap new Rails API

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.2.1'
+gem 'rails-api'
+gem 'puma'
+gem 'pg'
+
+group :development do
+  gem 'spring'
+  gem 'pry'
+  gem 'pry-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+ForamApi::Application.config.secret_token = 'ee21921ac9f1c52e02695a5332958c6faf6642dd02a260d886266f309b9b0012b26463ca74dba8246a147a989f37d67969cac6700664168e25fa9ba5d9809a85'
