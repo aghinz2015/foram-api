378e80499067ee34ccacd003c429e53a02aa02da
Maciej Gawel <magawel@student.agh.edu.pl>
2015-07-20 21:57:40 +0200

    Foram filter model cleanups

--- app/models/foram_filter.rb
+++ app/models/foram_filter.rb
@@ -1,32 +1,3 @@
-# class ForamFilter
-#   include ActiveModel::Model
-#   ATTRIBUTES = %w(deviation_angle_min         deviation_angle_max
-#                   growth_factor_min           growth_factor_max
-#                   rotation_angle_min          rotation_angle_max
-#                   translation_factor_min      translation_factor_max
-#                   wall_thickness_factor_min   wall_thickness_factor_max).freeze
-#   attr_accessor *ATTRIBUTES
-
-#   def forams
-#     @forams ||= find_forams
-#   end
-
-#   private
-
-#   def find_forams
-#     conditions = {}
-#     ATTRIBUTES.each do |attribute|
-#       conditions.deep_merge!(number_condition(attribute)) if send(attribute).present?
-#     end
-#     Foram.where(conditions)
-#   end
-
-#   def number_condition(key)
-#     operator = key =~ /_min\z/ ? '$gte' : '$lte'
-#     { "genotype.#{key[0..-5].camelize(:lower)}": { operator => send(key) } }
-#   end
-# end
-
 class ForamFilter
   include ActiveModel::Model
   include RangedFilter
--- lib/filters/ranged_filter.rb
+++ lib/filters/ranged_filter.rb
@@ -3,27 +3,22 @@ module RangedFilter
     base.extend(ClassMethods)
   end
 
-  def ranged_attributes_scope(scope) # scope = Foram.all
+  def ranged_attributes_scope(scope) # default scope = Foram.all
     conditions = {}
     self.class.ranged_attribute_names.each do |name|
-      ['_min', '_max'].each do |suffix|
+      { "_min" => "$gte", "_max" => "$lte" }.each_pair do |suffix, operator|
         attribute = name.split('.').last + suffix
         if send(attribute).present?
-          conditions.deep_merge!(number_condition(name, attribute))
+          conditions.deep_merge!("#{name.camelize(:lower)}": { operator => send(attribute) })
         end
       end
     end
     scope.where(conditions)
   end
 
-  def number_condition(name, attribute)
-    operator = attribute =~ /_min\z/ ? '$gte' : '$lte'
-    { "#{name.camelize(:lower)}": { operator => send(attribute) } }
-  end
-
   module ClassMethods
     def ranged_attribute(names)
-      @ranged_attribute_names = []
+      @ranged_attribute_names ||= []
       names.each do |name|
         ['_min', '_max'].each do |suffix|
           attr_accessor name.split('.').last + suffix
