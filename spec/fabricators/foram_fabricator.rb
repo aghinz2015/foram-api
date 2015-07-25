dd283ab8cd77dc89290ebb65dc5b03151f15f16a
fanfilmu <fanflm@gmail.com>
2015-07-25 21:17:09 +0200

    Refactor Foram/Genotype fabricators

--- spec/fabricators/foram_fabricator.rb
+++ spec/fabricators/foram_fabricator.rb
@@ -1,22 +1,5 @@
-require_relative "../support/fabricator_helper"
-
-Fabricator(:genotype) do
-  transient is_diploid: false
-
-  after_build do |genotype, transients|
-    fields = FabricatorHelper.attributes_for('foram')
-
-    fields.keys.each do |field|
-      range = fields[field][:range]
-      genotype.send("#{field}=", Fabricate(:gene, range: range, is_diploid: transients[:is_diploid]))
-    end
-  end
-
-  foram
-end
-
 Fabricator(:foram) do
-  className         { 'pl.edu.agh.evolutus.statistics.model.ForamFossil' }
+  className         'pl.edu.agh.evolutus.statistics.model.ForamFossil'
   foramId           { Faker::Lorem.characters(32) }
   deathStepNo       { Faker::Number.number(5) }
   age               { Faker::Number.number(4) }
@@ -26,7 +9,5 @@ Fabricator(:foram) do
   z                 { Faker::Number.number(1) }
   simulationStart   { Time.now - rand(50) }
 
-  after_build do |foram|
-    foram.genotype = Fabricate.build(:genotype, foram: foram, is_diploid: foram.isDiploid)
-  end
+  genotype          { |foram| Fabricate.build(:genotype, is_diploid: foram[:isDiploid]) }
 end
---
+++ spec/fabricators/genotype_fabricator.rb
@@ -0,0 +1,14 @@
+require_relative "../support/fabricator_helper"
+
+Fabricator(:genotype) do
+  transient is_diploid: false
+
+  after_build do |genotype, transients|
+    fields = FabricatorHelper.attributes_for('foram')
+
+    fields.keys.each do |field|
+      range = fields[field][:range]
+      genotype.send("#{field}=", Fabricate(:gene, range: range, is_diploid: transients[:is_diploid]))
+    end
+  end
+end
