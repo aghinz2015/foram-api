26a61219f0483af5c5e387ca8b1b325401298501
Bartosz Zurkowski <b.zurkowski93@gmail.com>
2015-08-07 01:25:51 +0200

    Remove unused actions from foram controller

--- app/controllers/v1/forams_controller.rb
+++ app/controllers/v1/forams_controller.rb
@@ -1,6 +1,6 @@
 module V1
   class ForamsController < ApplicationController
-    before_action :set_foram, only: [:show, :update, :destroy]
+    before_action :set_foram, only: :show
 
     def index
       @forams = Foram.all
@@ -9,23 +9,13 @@ module V1
     end
 
     def show
-      render json: @foram, root: true
-    end
-
-    def destroy
-      @foram.destroy
-
-      head :no_content
+      render json: @foram
     end
 
     private
 
     def set_foram
       @foram = Foram.find(params[:id])
-    end
-
-    def foram_params
-      params.require(:foram).permit(:kx, :ky, :kz, :tf, :phi, :beta)
     end
   end
 end
