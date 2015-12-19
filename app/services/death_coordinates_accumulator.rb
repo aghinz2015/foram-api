236d705fcb29b097426038f31b836dbbe444e156
KrezelewskiM <michalkrezelewski@gmail.com>
2015-12-19 15:25:28 +0100

    Optimize queries

--- app/services/death_coordinates_accumulator.rb
+++ app/services/death_coordinates_accumulator.rb
@@ -16,19 +16,57 @@ class DeathCoordinatesAccumulator
   private
 
   def accumulate_death_coordinates
+    range_hash = {
+      x_min: forams.min(:x), x_max: forams.max(:x),
+      y_min: forams.min(:y), y_max: forams.max(:y),
+    }
+
     if type == :three_dimensions
-      counters = Hash.new(0)
+      counters = {}
+      range_hash[:z_min] = forams.min(:z)
+      range_hash[:z_max] = forams.max(:z)
 
-      forams.each { |foram| counters[[foram.x, foram.y, foram.z]] += 1 }
+      map = %Q{
+        function() {
+          emit({x: this.x, y: this.y, z: this.z}, 1)
+        }
+      }
 
-      formatted_counters_for_3d(counters)
+      reduce = %Q{
+        function(key, values) {
+          return Array.sum(values);
+        }
+      }
+
+      result = forams.map_reduce(map, reduce).out(inline: true)
+      result.each { |entry| counters[entry["_id"].values] = entry["value"] }
+      formatted_counters_for_3d(counters).merge!(range_hash)
     else
       counters = Hash.new { |hash, key| hash[key] = Hash.new(0) }
 
-      z_param = type == :bubble ? :z : :death_hour
-      forams.each { |foram| counters[[foram.x, foram.y]][foram.send(z_param)] += 1 }
+      z_param = type == :bubble ? :z : :deathHour
+      range_hash[:z_min] = forams.min(z_param)
+      range_hash[:z_max] = forams.max(z_param)
 
-      formatted_counters_for_bubbles(counters)
+      map = %Q{
+        function() {
+          emit({x: this.x, y: this.y, z: this.#{z_param}}, 1)
+        }
+      }
+
+      reduce = %Q{
+        function(key, values) {
+          return Array.sum(values);
+        }
+      }
+
+      result = forams.map_reduce(map, reduce).out(inline: true)
+      result.each do |entry|
+        values = entry["_id"].values
+        counters[[values[0], values[1]]][values[2]] = entry["value"]
+      end
+
+      formatted_counters_for_bubbles(counters, range_hash[:z_min], range_hash[:z_max]).merge!(range_hash)
     end
   end
 
@@ -36,52 +74,29 @@ class DeathCoordinatesAccumulator
     result = []
     sum = counters.values.sum
 
-    x_set = Set.new
-    y_set = Set.new
-    z_set = Set.new
-
     counters.each do |(x, y, z), size|
-      x_set << x
-      y_set << y
-      z_set << z
-
       fraction = size / sum.to_f
       radius = fraction * DEFAULT_RADIUS * ENLARGING_FACTOR
 
       result << { x: x, y: y, z: z, size: size, marker: { radius: radius } }
     end
 
-    {
-      x_min: x_set.min, x_max: x_set.max,
-      y_min: y_set.min, y_max: y_set.max,
-      z_min: z_set.min, z_max: z_set.max,
-      data: result
-    }
+    { data: result }
   end
 
-  def formatted_counters_for_bubbles(counters)
+  def formatted_counters_for_bubbles(counters, z_min, z_max)
     result = []
 
-    z_array = counters.values.map(&:keys).flatten(1).uniq.sort
-    z_range = (z_array.first.to_i..z_array.last.to_i)
-
-    #because we might use limited forams collection, we dont't use global model #min and #max
-    x_set = Set.new
-    y_set = Set.new
+    z_range = (z_min.to_i..z_max.to_i)
 
     counters.each do |(x, y), z_hash|
-      x_set << x
-      y_set << y
-
       entry = { name: "x: #{x}, y: #{y}" }
       entry[:x] = z_range.map { |z| [z, x] }
       entry[:y] = z_range.map { |z| [z, y] }
-      entry[:size] = z_range.map { |z| [z, z_hash[z] || 0] }
+      entry[:size] = z_range.map { |z| [z, z_hash[z.to_f] || 0] }
       result << entry
     end
 
-    { data: result, z_min: z_range.first, z_max: z_range.last,
-                    x_min: x_set.min, x_max: x_set.max,
-                    y_min: y_set.min, y_max: y_set.max }
+    { data: result }
   end
 end
