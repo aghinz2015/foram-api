64ec552e1a92dc301b4c13896ed5e384c3af871c
KrezelewskiM <michalkrezelewski@gmail.com>
2015-12-20 15:49:39 +0100

    Further optimizations

--- app/services/death_coordinates_accumulator.rb
+++ app/services/death_coordinates_accumulator.rb
@@ -18,7 +18,7 @@ class DeathCoordinatesAccumulator
   def accumulate_death_coordinates
     range_hash = {
       x_min: forams.min(:x), x_max: forams.max(:x),
-      y_min: forams.min(:y), y_max: forams.max(:y),
+      y_min: forams.min(:y), y_max: forams.max(:y)
     }
 
     if type == :three_dimensions
@@ -28,7 +28,7 @@ class DeathCoordinatesAccumulator
 
       map = %Q{
         function() {
-          emit({x: this.x, y: this.y, z: this.z}, 1)
+          emit({x: this.x, y: this.y, z: this.z}, 1);
         }
       }
 
@@ -39,7 +39,7 @@ class DeathCoordinatesAccumulator
       }
 
       result = forams.map_reduce(map, reduce).out(inline: true)
-      result.each { |entry| counters[entry["_id"].values] = entry["value"] }
+      result.each { |entry| counters[entry['_id'].values.map(&:to_i)] = entry['value'].to_i }
       formatted_counters_for_3d(counters).merge!(range_hash)
     else
       counters = Hash.new { |hash, key| hash[key] = Hash.new(0) }
@@ -50,7 +50,7 @@ class DeathCoordinatesAccumulator
 
       map = %Q{
         function() {
-          emit({x: this.x, y: this.y, z: this.#{z_param}}, 1)
+          emit({x: this.x, y: this.y, z: this.#{z_param}}, 1);
         }
       }
 
@@ -62,11 +62,11 @@ class DeathCoordinatesAccumulator
 
       result = forams.map_reduce(map, reduce).out(inline: true)
       result.each do |entry|
-        values = entry["_id"].values
-        counters[[values[0], values[1]]][values[2]] = entry["value"]
+        values = entry['_id'].values.map(&:to_i)
+        counters[[values[0], values[1]]][values[2]] = entry['value']
       end
 
-      formatted_counters_for_bubbles(counters, range_hash[:z_min], range_hash[:z_max]).merge!(range_hash)
+      formatted_counters_for_bubbles(counters).merge!(range_hash)
     end
   end
 
@@ -84,16 +84,14 @@ class DeathCoordinatesAccumulator
     { data: result }
   end
 
-  def formatted_counters_for_bubbles(counters, z_min, z_max)
+  def formatted_counters_for_bubbles(counters)
     result = []
-
-    z_range = (z_min.to_i..z_max.to_i)
 
     counters.each do |(x, y), z_hash|
       entry = { name: "x: #{x}, y: #{y}" }
-      entry[:x] = z_range.map { |z| [z, x] }
-      entry[:y] = z_range.map { |z| [z, y] }
-      entry[:size] = z_range.map { |z| [z, z_hash[z.to_f] || 0] }
+      entry[:x] = x
+      entry[:y] = y
+      entry[:size] = z_hash.to_a
       result << entry
     end
 
