c50f7f9976f101e14cf333b7274bacbc2d874407
Maciej Gawel <m.gawel@hotmail.com>
2015-12-31 14:20:29 +0100

    Fix foram filter attribute names

--- app/controllers/v1/foram_filters_controller.rb
+++ app/controllers/v1/foram_filters_controller.rb
@@ -37,7 +37,7 @@ module V1
       only_numeric = params[:only_numeric] == 'true' ? true : false
       only_genotype = params[:only_genotype] == 'true' ? true : false
       render json: Foram.filterable_attribute_names(user: current_user,
-                                                    only_numeric: only_numeric
+                                                    only_numeric: only_numeric,
                                                     only_genotype: only_genotype),
              root: :attributes
     end
