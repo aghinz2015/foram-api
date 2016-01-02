ca0bde0bef182a01c6acdb68e47c08ca2a3dd1dd
Maciej Gawel <m.gawel@hotmail.com>
2016-01-02 20:38:00 +0100

    Add filters to foram scoping

--- app/controllers/concerns/foram_scoping.rb
+++ app/controllers/concerns/foram_scoping.rb
@@ -2,6 +2,14 @@ module ForamScoping
   extend ActiveSupport::Concern
 
   def foram_scope
-    Foram.for_user(current_user).simulation_start(params[:simulation_start])
+    foram_filter = ForamFilter.new(foram_scope_params)
+    foram_filter.forams(user: current_user).simulation_start(params[:simulation_start])
+  end
+
+  private
+
+  def foram_scope_params
+    attributes = ForamFilter.attributes_map(Foram.for_user(current_user)).keys
+    params.slice(*attributes).to_hash
   end
 end
